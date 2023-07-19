4e9fad05e2997a84cd8a652c3e1b14d7c7a813bf
Gabriel Andrade Almeida <59920810+g-andradd@users.noreply.github.com>
2023-07-19 15:40:15 -0300

    :wrench: chore: ambiente dev e banco de dados h2 configurado

--- src/main/java/br/com/alura/jornadamilhas/JornadaMilhasApplication.java
+++ src/main/java/br/com/alura/jornadamilhas/JornadaMilhasApplication.java
@@ -2,7 +2,9 @@ package br.com.alura.jornadamilhas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
+@Profile("dev")
 @SpringBootApplication
 public class JornadaMilhasApplication {
 
